ce5f3b79468784382c2a8f014ab9bf808c7502dc
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-12 15:44:14 +0100

    Initial Commit

---
+++ second summary/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  second summary
+//
+//  Created by Жека Г on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
